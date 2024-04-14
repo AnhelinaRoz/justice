f7062e1f4daf07b596beb73524657d47397a77e0
AnhelinaRoz <121691147+AnhelinaRoz@users.noreply.github.com>
2024-04-14 21:00:46 +0200

    more php

--- php_scripts/sendemail.php
+++ php_scripts/sendemail.php
@@ -1,7 +1,21 @@
 <?php
+$firstname = $_POST["firstname"];
+$lastname = $_POST["lastname"];
+$email = $_POST["email"];
+$number = $_POST["number"];
+$message = $_POST["message"];
+//$ = $_POST[""];
+//$ = $_POST[""];
+
 $to = "rozkovanangelina@gmail.com";
-$subject = "Тема сообщения";
-$message = "Привет, это текст сообщения!";
+$subject = "Email from the website";
+$message = "
+<p>$firstname</p>
+<p>$lastname</p>
+<p>$email</p>
+<p>$number</p>
+<p>$message</p>
+";
 $headers = "From: sender@example.com" . "\r\n" .
     "Reply-To: sender@example.com" . "\r\n" .
     "X-Mailer: PHP/" . phpversion();

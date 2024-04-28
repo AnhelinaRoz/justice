8cf40404df3d9cef06491e6263b568c89cc29d89
AnhelinaRoz <121691147+AnhelinaRoz@users.noreply.github.com>
2024-04-28 20:55:59 +0200

    img

--- swipe_slider.js
+++ swipe_slider.js
@@ -12,3 +12,16 @@ var swiper = new Swiper(".mySwiper", {
     //     prevEl: ".swiper-button-prev",
     // },
 });
+var swiper2 = new Swiper(".mySwiper_img", {
+    slidesPerView: 1,
+    loop: true,
+    autoplay: {
+
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    // navigation: {
+    //     nextEl: ".swiper-button-next",
+    //     prevEl: ".swiper-button-prev",
+    // },
+});

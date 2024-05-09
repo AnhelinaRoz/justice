437ac20b6802d87b3160b56804a4e8bd168ac938
AnhelinaRoz <121691147+AnhelinaRoz@users.noreply.github.com>
2024-05-09 21:33:41 +0200

    videos change

--- index.php
+++ index.php
@@ -472,12 +472,12 @@ $get_press_release = CustomQuery("SELECT * FROM `news` ORDER BY  `date` DESC LIM
                 <div class="swiper-wrapper">
                     <div class="swiper-slide">
                         <video controls class="video">
-                            <source src="img/video1.mp4" type="video/mp4">
+                            <source src="img/video2.mp4" type="video/mp4">
                         </video>
                     </div>
                     <div class="swiper-slide">
                         <video controls class="video">
-                            <source src="img/video2.mp4" type="video/mp4">
+                            <source src="img/video1.mp4" type="video/mp4">
                         </video>
                     </div>
                 </div>
